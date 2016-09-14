4779e924968f32385a3fade7a23d242f9941d50e
Mark Mendoza <Mark.Mendoza@iqmetrix.com>
2016-09-14 16:48:29 +0800

    Initial Commit

---
+++ TourCity/TourCity.Web/TourCity.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TourCity.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TourCity/TourCity.Web/TourCity.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TourCity.Web.WebApiApplication" Language="C#" %>
