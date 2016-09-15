53df750d705200fb941012a1ebc936d606e95fea
Mark Mendoza <markjaysonmendoza@gmail.com>
2016-09-15 15:34:13 +0800

    added routing

--- TourCity/TourCity.Web/TourCity.Web/content/js/index.js
+++ TourCity/TourCity.Web/TourCity.Web/content/js/index.js
@@ -1,11 +1,28 @@
-﻿var tourcityapp = angular.module('tourcityapp', []);
+﻿//main app module
+var tourcityapp = angular.module('tourcityapp', ['ngRoute']);
 
+//Controller
 var maincontroller = tourcityapp.controller('maincontroller',
     function ($scope) {
 
         $scope.Name = "TestName";
-        $scope.Categories = [{Name: "guagua"}, { Name: "lubao" }];
+        $scope.Categories = [{ Name: "guagua" }, { Name: "lubao" }];
 
     });
 
+//Routes
+tourcityapp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/',
+        {
+            template: 'Welcome user!'
+        })
+        .when('/anotherpage',
+        {
+            template: 'Welcome user, again!'
+        })
+        .otherwise({
+            template: '404'
+        });
+}]);
 
