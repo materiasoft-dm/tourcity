0cf76727378c991b16a73bdff445cdaeb7d41616
Mark Mendoza <markjaysonmendoza@gmail.com>
2016-09-15 13:55:11 +0800

    removed unecessary files for starting from scratch

--- TourCity/TourCity.Web/TourCity.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TourCity.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- TourCity/TourCity.Web/TourCity.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TourCity.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
