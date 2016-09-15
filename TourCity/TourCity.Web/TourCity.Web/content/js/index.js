738d68e02b69f21f1978f49c027d8ceed77abd99
Mark Mendoza <markjaysonmendoza@gmail.com>
2016-09-15 15:16:01 +0800

    first controller sample

---
+++ TourCity/TourCity.Web/TourCity.Web/content/js/index.js
@@ -0,0 +1,11 @@
+﻿var tourcityapp = angular.module('tourcityapp', []);
+
+var maincontroller = tourcityapp.controller('maincontroller',
+    function ($scope) {
+
+        $scope.Name = "TestName";
+        $scope.Categories = [{Name: "guagua"}, { Name: "lubao" }];
+
+    });
+
+
