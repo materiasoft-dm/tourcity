53617885ef2ec39ffb4e80e94b2347a7e6081b24
Mark Mendoza <markjaysonmendoza@gmail.com>
2016-09-16 10:11:30 +0800

    Data annotations on model. TODO: create fluent codefirst model configuration

--- TourCity/TourCity.Web/TourCity.Repository/Class1.cs
+++ TourCity/TourCity.Web/TourCity.Repository/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
@@ -31,6 +32,8 @@ namespace TourCity.Repository
 
     public class TestType
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
     }
