be7547dbf92bf68bf9d2ed367f07d4f5d3a69ab7
Mark Mendoza <markjaysonmendoza@gmail.com>
2016-09-19 12:09:49 +0800

    Added login functionality - not yet connected to the database

--- TourCity/TourCity.Web/TourCity.Web/Global.asax.cs
+++ TourCity/TourCity.Web/TourCity.Web/Global.asax.cs
@@ -16,6 +16,10 @@ namespace TourCity.Web
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            AreaRegistration.RegisterAllAreas();
+
         }
     }
 }
