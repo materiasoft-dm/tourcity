b69a45a43fc5ff30cd21188a761b99068161011c
Mark Mendoza <markjaysonmendoza@gmail.com>
2016-09-19 11:21:43 +0800

    Added UserController - prerequisite for user registration

---
+++ TourCity/TourCity.Web/TourCity.Web/Models/Accounts/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourCity.Web.Models.Accounts
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+
+    }
+}
