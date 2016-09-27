e8b4f42ed897be679834faeb6a79b3ae13dfb5d7
Mark Mendoza <markjaysonmendoza@gmail.com>
2016-09-27 11:13:01 +0800

    Basic Google Map widget added

---
+++ TourCity/TourCity.Web/TourCity.Web/Areas/Widgets/WidgetsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TourCity.Web.Areas.Widgets
+{
+    public class WidgetsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Widgets";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Widgets_default",
+                "Widgets/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
--- TourCity/TourCity.Web/TourCity.Web/Global.asax.cs
+++ TourCity/TourCity.Web/TourCity.Web/Global.asax.cs
@@ -14,12 +14,14 @@ namespace TourCity.Web
     {
         protected void Application_Start()
         {
+            AreaRegistration.RegisterAllAreas();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
 
-            AreaRegistration.RegisterAllAreas();
+            
             AutomapperConfig.Init();
             
 
